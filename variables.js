b0d17f6dc59f176e4ef0666fed8d69a0264f8860
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-07 11:37:17 +0530

    Added code

---
+++ variables.js
@@ -0,0 +1,8 @@
+const acc_Id = 123456789
+let acc_Name = "Himanish"
+var acc_Password = "Himanish@123"
+
+// acc_Id=9
+console.log("Account Id: " + acc_Id);
+console.log("Account Name: " + acc_Name);
+console.log("Account Password: " + acc_Password);
