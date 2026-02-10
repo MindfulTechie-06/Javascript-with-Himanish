6c709b4e8295554b5315f56bc16aac895d624493
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-10 18:55:04 +0530

    Some more conversions

--- Conversion_Operation.js
+++ Conversion_Operation.js
@@ -4,3 +4,30 @@ let score ="33abc"
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber );
+
+// "33" => 33
+// "33abc" => NaN
+// "abc33" => NaN
+// "abc" => NaN
+// "" => 0
+// true => 1
+// false => 0   
+
+let isLoggedIn = 211;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 0 => false
+// 1 => true    
+// "" => false
+// " " => true
+// "false" => true
+// null => false
+// undefined => false
+
+// No to String Conversion
+let score1 = 33;
+let scoreInString = String(score1);
+console.log(scoreInString);
+console.log(typeof scoreInString);
