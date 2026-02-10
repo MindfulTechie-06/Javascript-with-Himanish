c543715db8a2cd838b627f5c04c14c0aa5c385c7
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-10 19:04:01 +0530

    Basic Arithmetric Operations

--- Operations.js
+++ Operations.js
@@ -4,3 +4,39 @@
 let val=3;
 let negVal=-val;
 console.log(negVal);
+
+//2.Addition
+let a=10;
+let b=20;
+let sum=a+b;
+console.log(sum);
+
+//3.Subtraction
+let diff=a-b;
+console.log(diff);
+
+//4.Multiplication
+let product=a*b;
+console.log(product);
+
+//5.Division
+let div=a/b;
+console.log(div);
+
+//6.Modulus
+let mod=a%b;
+console.log(mod);
+
+//7.Increment
+let inc=a++;
+console.log(inc);
+console.log(a);
+
+//8.Decrement
+let dec=b--;
+console.log(dec);
+console.log(b);
+
+//9.Exponentiation
+let exp=a**2;
+console.log(exp);
