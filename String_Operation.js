7dc298ad7dc4b47bce0c3715d5f7796989a04a19
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-11 19:54:05 +0530

    Created Compariso.js File

--- String_Operation.js
+++ String_Operation.js
@@ -4,3 +4,6 @@ console.log("1"+ 2);
 console.log("1"+2+2);//This is because of the left to right evaluation of the expression. When the first two operands are numbers, it performs addition. But when it encounters a string, it converts the number to a string and performs concatenation for the rest of the expression.
 console.log(1+2+"2");
 console.log(1+"2"+2);//This is because of the left to right evaluation of the expression. When the first two operands are numbers, it performs addition. But when it encounters a string, it converts the number to a string and performs concatenation for the rest of the expression.
+console.log(typeof true);
+console.log(true+1);
+
