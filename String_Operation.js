c7f1d3db31362ebc7f7ff8b55f3e7c909d4c6bf6
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-11 19:22:23 +0530

    Performed Simple Operations of String Addition

---
+++ String_Operation.js
@@ -0,0 +1,6 @@
+console.log("String Operations in JavaScript");
+console.log(1+ "2");
+console.log("1"+ 2);
+console.log("1"+2+2);//This is because of the left to right evaluation of the expression. When the first two operands are numbers, it performs addition. But when it encounters a string, it converts the number to a string and performs concatenation for the rest of the expression.
+console.log(1+2+"2");
+console.log(1+"2"+2);//This is because of the left to right evaluation of the expression. When the first two operands are numbers, it performs addition. But when it encounters a string, it converts the number to a string and performs concatenation for the rest of the expression.
