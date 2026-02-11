bd9dfc27f2c8582d96780f5637cb6cf5f4aa79cb
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-11 20:01:10 +0530

    Some Basic Comparisons

---
+++ Comparison.js
@@ -0,0 +1,24 @@
+console.log("Some Comparison Operators in JavaScript");
+
+//1.Equality
+let x=10;
+let y="10";
+console.log(x==y); //true because it performs type coercion
+console.log(x===y); //false because it checks for both value and type
+
+//2.Inequality
+console.log(x!=y); //false because it performs type coercion
+console.log(x!==y); //true because it checks for both value and type
+
+//3.Greater than
+console.log(x>5); //true
+
+//4.Less than
+console.log(x<20); //true
+
+//5.Greater than or equal to
+console.log(x>=10); //true
+
+//6.Less than or equal to
+console.log(x<=10); //true
+
