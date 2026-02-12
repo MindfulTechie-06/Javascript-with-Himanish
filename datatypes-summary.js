8e716d655068de99f02921a6e30f48034049f570
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-12 20:56:48 +0530

    Added Notes

---
+++ datatypes-summary.js
@@ -0,0 +1,6 @@
+// Primitive data types in JavaScript include:
+
+// They Are all Call By Value meaning when they are called in a function, a copy of the value is passed to the function, and any changes made to the parameter inside the function do not affect the original variable outside the function.
+
+// 7 primitive data types in JavaScript are:
+// String, Number, BigInt, Boolean, Undefined, Null, Symbol, BigInt
