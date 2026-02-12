5a4587edce4293c9ec4343817b23806b7111ec0b
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-12 20:58:08 +0530

    Extra

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -4,3 +4,6 @@
 
 // 7 primitive data types in JavaScript are:
 // String, Number, BigInt, Boolean, Undefined, Null, Symbol, BigInt
+
+
+//  Reference data types in JavaScript include:// They Are all Call By Reference meaning when they are called in a function, a reference to the original variable is passed to the function, and any changes made to the parameter inside the function will affect the original variable outside the function. // 3 reference data types in JavaScript are: // Object, Array, Function
