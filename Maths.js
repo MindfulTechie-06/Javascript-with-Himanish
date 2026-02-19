c3a8a4e91eb4c2aad3e4a6c160cc74be87b6a779
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-19 20:19:27 +0530

    Created a new file maths.js and took some examples of methods

---
+++ Maths.js
@@ -0,0 +1,7 @@
+// we will be studying some maths methods in this file
+
+// Math is a built-in object in JavaScript that provides properties and methods for mathematical constants and functions. It is not a function object, so it cannot be instantiated.
+
+console.log(Math);
+let value = Math.PI;
+console.log(value.toPrecision(3)); // Output: 3.141592653589793
