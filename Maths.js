0f58388db0469117b876413cdcca3875365e23ba
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-20 21:25:02 +0530

    Added Max and Min Function

--- Maths.js
+++ Maths.js
@@ -5,3 +5,13 @@
 console.log(Math);
 let value = Math.PI;
 console.log(value.toPrecision(3)); // Output: 3.141592653589793
+
+
+console.log(Math.abs(-5)); // Output: 5
+console.log(Math.ceil(4.2)); // Output: 5
+console.log(Math.floor(4.7)); // Output: 4
+console.log(Math.round(4.5)); // Output: 5
+
+
+console.log(Math.max(1, 5, 3)); // Output: 5
+console.log(Math.min(1, 5, 3)); // Output: 1
