7f17215dada42cb0586011749c4a05c7bed9db64
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-24 20:49:43 +0530

    Added All Mostly used Methods of Dates

--- Date_Time.js
+++ Date_Time.js
@@ -16,3 +16,6 @@ console.log(mydate.toDateString());
 
 let newdate= new Date(2024, 0, 1,5,3);  // created a Date object representing January 1, 2024. Note that the month is zero-indexed (0 = January)>
 console.log(newdate.toLocaleString());
+
+let date1 = new Date(12-12-2025);
+console.log(date1.toLocaleString());// This will not give the expected result because the expression 12-12-2025 is evaluated as a mathematical operation, resulting in -2025. When passed to the Date constructor, it creates a date based on the number of milliseconds since January 1, 1970, which is not the intended date.
