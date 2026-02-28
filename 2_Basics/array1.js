28dd4df3ec22a661bce980a174d82c6a07a400b7
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-02-28 22:02:10 +0530

    Added An Example OF Array

---
+++ 2_Basics/array1.js
@@ -0,0 +1,9 @@
+// array is a data structure that can hold more than one value at a time. It is a collection of items stored at contiguous memory locations. The idea is to store multiple items of the same type together. This makes it easier to calculate the position of each element by simply adding an offset to a base value, i.e., the memory location of the first element of the array (generally denoted by the name of the array).
+
+// In JavaScript, arrays are dynamic and can hold elements of any type. They are created using square brackets [] and can be accessed using their index, which starts from 0.
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+// Accessing elements of the array
+console.log(fruits[0]); // Output: Apple
+console.log(fruits[1]); // Output: Banana
+console.log(fruits[2]); // Output: Cherry
