58f62d58ac57ae8d838fbf1a4842c0a021db5748
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-10 20:46:21 +0530

    Example of using flat method

--- 2_Basics/array2.js
+++ 2_Basics/array2.js
@@ -9,3 +9,7 @@ console.log(marverl_heroes); // Output
 // Concat() - is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
 const all_heroes = marverl_heroes.concat(dc_heroes);
 console.log(all_heroes); 
+
+const another_array=[1,2,3[4,5,6],7,8,9,[98,99,100]];
+const flat_array = another_array.flat();
+console.log(flat_array); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99, 100]
