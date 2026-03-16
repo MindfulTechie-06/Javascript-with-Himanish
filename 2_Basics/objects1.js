a6119929dc4cd041f4e9a512143b6065f7f74eae
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-16 20:34:25 +0530

    Simple Program on object

--- 2_Basics/objects1.js
+++ 2_Basics/objects1.js
@@ -1,2 +1,22 @@
 // objects are collections of properties
 // properties are key-value pairs   
+// Simple Object Example
+
+let student = {
+    name: "Himanish",
+    age: 20,
+    marks: 85,
+
+    displayInfo: function () {
+        console.log("Student Name:", this.name);
+        console.log("Age:", this.age);
+        console.log("Marks:", this.marks);
+    }
+};
+
+// Access properties
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+
+// Call object method
+student.displayInfo();
