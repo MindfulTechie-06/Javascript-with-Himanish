3128a254415ad9a779481d75f637975febf25594
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-17 19:44:24 +0530

    Added a symbol

--- 2_Basics/objects1.js
+++ 2_Basics/objects1.js
@@ -2,11 +2,14 @@
 // properties are key-value pairs   
 // Simple Object Example
 
+// declaring a symbol
+const mysym=Symbol("key 1");
 
 
 const student = {
     name: "Himanish",
     age: 20,
+    [mysym]:"This is a symbol",
     marks: 85,
     isPassed: true,
     lastlogindays: ["Monday", "Wednesday", "Friday"],
@@ -17,3 +20,5 @@ const student = {
 console.log("Name:", student.name);
 console.log("Age:", student.age);
 console.log(typeof(student.isPassed));
+// Accessing symbol property
+console.log("Symbol Property:", student[mysym]);
