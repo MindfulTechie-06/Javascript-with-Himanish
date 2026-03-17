87b86cd4eebff8dab8fcb8c262ddff37d5d8a56b
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-17 19:40:53 +0530

    Added Some more keys

--- 2_Basics/objects1.js
+++ 2_Basics/objects1.js
@@ -2,21 +2,18 @@
 // properties are key-value pairs   
 // Simple Object Example
 
-let student = {
+
+
+const student = {
     name: "Himanish",
     age: 20,
     marks: 85,
-
-    displayInfo: function () {
-        console.log("Student Name:", this.name);
-        console.log("Age:", this.age);
-        console.log("Marks:", this.marks);
-    }
+    isPassed: true,
+    lastlogindays: ["Monday", "Wednesday", "Friday"],
+    
 };
 
 // Access properties
 console.log("Name:", student.name);
 console.log("Age:", student.age);
-
-// Call object method
-student.displayInfo();
+console.log(typeof(student.isPassed));
