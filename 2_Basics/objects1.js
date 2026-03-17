e2d07adfa0c0452b6ca3c403cc77dfcdb06ba468
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-17 20:02:45 +0530

    Auto-sync: file changes

--- 2_Basics/objects1.js
+++ 2_Basics/objects1.js
@@ -22,3 +22,10 @@ console.log("Age:", student.age);
 console.log(typeof(student.isPassed));
 // Accessing symbol property
 console.log("Symbol Property:", student[mysym]);
+
+// changing the value of a property
+student.marks = 90;
+Object.freeze(student); // This will prevent any changes to the student object
+student.marks = 95; // This will not change the marks property
+console.log(student);
+
