99f405bdc33d1f645e24a673dd859f5c2f15ffae
MindfulTechie-06 <himanishchatterjee060@gmail.com>
2026-03-27 21:11:24 +0530

    Created another object program

---
+++ 2_Basics/obj_array.js
@@ -0,0 +1,9 @@
+// Object and Array Manipulation
+// This code demonstrates how to work with objects and arrays in JavaScript, including creating objects, accessing properties, modifying properties, and working with arrays of objects. It also includes examples of using functions to manipulate data within these structures.
+
+// Example of an object representing a student
+const student = {
+    name: "Himanish",
+    age: 20,
+    marks: 85,
+    email: "",}
